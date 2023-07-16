da904a29e21e82f6450e49cd9a496fef47f5e058
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-16 23:21:01 +0530

    Create Merge Two Sorted Array.java

---
+++ Merge Two Sorted Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       int i=m-1;
+       int j=n-1;
+       int merge =m+n-1;
+
+       while(i>=0 && j>=0){
+           if(nums1[i]>nums2[j]){
+           nums1[merge] =nums1[i];
+           i--;
+           }
+           else{
+               nums1[merge] =nums2[j];
+               j--;
+           }
+             merge--;
+       }
+       while(j>=0){
+           nums1[merge] = nums2[j];
+           j--;
+           merge--;
+       }
+    }
+}
