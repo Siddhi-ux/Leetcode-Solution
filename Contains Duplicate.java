82dc38b56ff619b6237fd727650c73a37b98197a
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-18 20:18:27 +0530

    Create Contains Duplicate.java

---
+++ Contains Duplicate.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        boolean res = containsDuplicate(nums);
+        System.out.println(res);
+    }
+}
