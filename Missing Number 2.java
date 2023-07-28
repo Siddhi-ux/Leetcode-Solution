44a0cf4ed569a379c43c1309bbf8a1ee1d92f045
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-28 16:36:53 +0530

    Update Missing Number 2.java

--- Missing Number 2.java
+++ Missing Number 2.java
@@ -1,14 +1,15 @@
 class Solution {
-    int missingNumber(int array[], int n) {
+    public int missingNumber(int[] nums) {
+       
         int e=0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=nums.length;i++){
             e^=i;
         }
         int a=0;
-        for(int num : array){
+        for(int num : nums){
             a^=num;
         }
         return a^e;
-    }
     
+    }
 }
