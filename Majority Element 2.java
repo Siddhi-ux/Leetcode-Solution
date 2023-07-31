d0df44074d91977006ea7919cdb638a9a46f9828
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-07-31 20:23:34 +0530

    Create Majority Element 2.java

---
+++ Majority Element 2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        
+
+        for (int num : countMap.keySet()) {
+            if (countMap.get(num) > n / 3) {
+                result.add(num);
+            }
+        }
+        
+        return result;
+    }
+}
