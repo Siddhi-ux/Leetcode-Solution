59e5ff6b2ec73ca08e44adab01c3772762964207
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-09-10 22:35:41 +0530

    Update 1161. Maximum Level Sum of a Binary Tree.java

--- 1161. Maximum Level Sum of a Binary Tree.java
+++ 1161. Maximum Level Sum of a Binary Tree.java
@@ -4,6 +4,7 @@ class Solution {
     int maxLevelSum = Integer.MIN_VALUE;
     Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
 
+    
     for (int level = 1; !q.isEmpty(); ++level) {
       int levelSum = 0;
       for (int sz = q.size(); sz > 0; --sz) {
