629a509dcccc8a54c01c84c15fbdfd50ea044930
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-09-15 20:07:50 +0530

    Create Balanced Binary Tree.java

---
+++ Balanced Binary Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        if( Height(root) == -1) return false;
+        return true;
+    }
+    public int Height(TreeNode root){
+        if(root == null) return 0;
+        int left =Height(root.left);
+        int right = Height(root.right);
+        if(left == -1 || right == -1) return -1;
+        if(Math.abs(left-right)>1) return -1;
+        return Math.max(left,right) +1;
+    }
+}
