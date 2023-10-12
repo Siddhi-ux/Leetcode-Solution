d7ab0526486596c4af89d122174110312d09dba2
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-10-12 23:33:42 +0530

    Update 557.java

--- 557.java
+++ 557.java
@@ -1,7 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
         String[] words = s.split(" ");
-        StringBuilder rs = new StringBuilder();
+        StringBuilder rs = new StringBuilder( );
         for(String word : words){
             StringBuilder rw = new StringBuilder(word).reverse();
             rs.append(rw).append(" ");
