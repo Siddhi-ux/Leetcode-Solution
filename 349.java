c601e47101d58ea9a2aa31f89b8fc8bdb460bf48
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-10-24 21:21:32 +0530

    Create 349.java

---
+++ 349.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int r=0;
+        int len=nums2.length;
+        int arr[] =new int[Math.min(len,n)];
+        boolean [] s=new boolean[1000];
+        for(int i:nums1){s[i]=true;}
+        for(int i:nums2){
+            if(s[i]){
+                arr[r]=i;
+                r++;
+                s[i]=false;
+            }
+        }
+        return Arrays.copyOfRange(arr,0,r);
+    }
+}
