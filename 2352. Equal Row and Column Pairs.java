b9db5c6469177f87f6ba4214ddae50ff7f022de3
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-10-25 22:23:37 +0530

    Update 2352. Equal Row and Column Pairs.java

--- 2352. Equal Row and Column Pairs.java
+++ 2352. Equal Row and Column Pairs.java
@@ -5,6 +5,7 @@ class Solution {
 
     for (int i = 0; i < n; ++i)
       for (int j = 0; j < n; ++j) {
+        
         int k = 0;
         for (; k < n; ++k)
           if (grid[i][k] != grid[k][j])
